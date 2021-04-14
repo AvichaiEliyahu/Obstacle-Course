39e6d478a5aa2f58dfa9045881879ee72d9b8c5d
AvichaiEliyahu <avihayboom@gmail.com>
2021-04-14 10:36:05 +0300

    initial commit

---
+++ Assets/Scripts/ObjectHit.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectHit : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        Component c = GetComponent<MeshRenderer>();
+        
+        Debug.Log("Bumped into a wall! ("+c.name+")");
+        if (collision.gameObject.tag == "Player")
+        {
+            GetComponent<MeshRenderer>().material.color = Color.red;
+            gameObject.tag = "Hit";
+        }
+    }
+}
